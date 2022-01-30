5f5cd6d466be1742429fd1bc1b1bf507749a20df
Md. Sazzad Talukder <sazzadtalukdercseiu@gmail.com>
2022-01-30 23:10:21 +0600

    Uploading My Templete1 Project

---
+++ src/MyComponent/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+function Footer() {
+    return ( 
+        <div>
+            <footer className="w3-center w3-light-grey w3-padding-32">
+  <p>Powered by <a href="https://github.com/sazzadtalukder" title="W3.CSS" target="_blank" className="w3-hover-text-green">-Md Sazzad Talukder</a></p>
+</footer>
+
+
+        </div>
+
+     );
+}
+
+export default Footer;
